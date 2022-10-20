8eb9b82e2628af84231f2aff85a057ed4e624b4c
taehee_Jung <palzmnbvcx@gmail.com>
2022-10-20 12:14:15 +0900

    machine learing success

--- project/settings.py
+++ project/settings.py
@@ -22,14 +22,16 @@ env = environ.Env(
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+#reading .env file
+environ.Env.read_env(
+    env_file= os.path.join(BASE_DIR, '.env')
+)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env('SECRET_KEY')
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
@@ -50,6 +52,7 @@ INSTALLED_APPS = [
     "project",
     "users",
     "post",
+    "sslserver",
 ]
 
 MIDDLEWARE = [
--- project/urls.py
+++ project/urls.py
@@ -20,6 +20,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("users/", include("users.urls")),
+    path("", include("users.urls")),
     path("posts/", include("post.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
